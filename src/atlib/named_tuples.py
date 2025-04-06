72e9c4bcfe72691edd441ea9b7e5f07c6a3a00b6
Chris L <stylesuxx@gmail.com>
2025-04-06 23:38:09 +0200

    Add named_tuple classes

---
+++ src/atlib/named_tuples.py
@@ -0,0 +1,22 @@
+from typing import NamedTuple
+
+
+class SignalQualityInfo(NamedTuple):
+    rsrp: int
+    rsrq: int
+
+
+class CellInfo(NamedTuple):
+    mcc: int
+    mnc: int
+    imsi: int
+    roaming_status: int
+    band: int
+    bandwidth_index: int
+    earfcn: int
+    cell_id: int
+    rsrp: int
+    rsrq: int
+    tac: int
+    signal_level: int
+    pcid: int
